4b611d88554d565848572e86d81295fa769e8374
funtalia <afarukg@gmail.com>
2019-03-19 15:26:10 +0300

    convert owners to JPA Entities.closes #36

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,7 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
 
+    @Column(name = "name")
     public String getName() {
         return name;
     }
